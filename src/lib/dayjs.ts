34c1f0cb450e9ac9299d7ec72735ebe02954d8cb
maarcos4g <marcospaullo552@gmail.com>
2025-07-14 13:03:01 -0300

    fix: correct locale casing for dayjs

--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -2,7 +2,7 @@ import lib from "dayjs"
 import relativeTime from "dayjs/plugin/relativeTime"
 import 'dayjs/locale/pt-BR'
 
-lib.locale('pt-BR')
 lib.extend(relativeTime)
+lib.locale('pt-br')
 
 export const dayjs = lib
